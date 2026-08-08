e80bb779936ac2c7677f23a2156388e8cf2c17a0
behzadbagheri8757-sudo <behzad.bagheri8757@gmail.com>
2026-08-08 12:59:26 +0330

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: 1,
+    name: "لوبیا چیتی",
+    unitPrice: 200000,
+    packPrice: 1000000,
+    image: "images/lobia.jpg",
+    order: 1,
+    available: true
+  },
+  {
+    id: 2,
+    name: "نخود",
+    unitPrice: 180000,
+    packPrice: 900000,
+    image: "images/nohood.jpg",
+    order: 2,
+    available: true
+  },
+  {
+    id: 3,
+    name: "عدس",
+    unitPrice: 220000,
+    packPrice: null,
+    image: "images/adas.jpg",
+    order: 3,
+    available: false
+  }
+];
