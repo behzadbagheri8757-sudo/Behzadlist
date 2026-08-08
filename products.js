a8d5980f83fa95f4bb029435a58abad7c5d928f9
behzadbagheri8757-sudo <behzad.bagheri8757@gmail.com>
2026-08-08 16:47:13 +0330

    Add files via upload

---
+++ products.js
@@ -0,0 +1,51 @@
+// فایل اطلاعات محصولات
+// برای تغییر قیمت‌ها یا اضافه کردن محصول، این فایل را ویرایش کنید
+// یا از پنل مدیریت (admin.html) استفاده کرده و فایل جدید را دانلود و جایگزین کنید
+
+const products = [
+  {
+    "id": 1,
+    "name": "لوبیا چیتی",
+    "unitPrice": 395000,
+    "packPrice": null,
+    "image": "Lobia-chiti.JPG",
+    "order": 1,
+    "available": true
+  },
+  {
+    "id": 2,
+    "name": "عدس ساسکن",
+    "unitPrice": 235000,
+    "packPrice": 1175000,
+    "image": "adas-saskan.jpg",
+    "order": 2,
+    "available": true
+  },
+  {
+    "id": 3,
+    "name": "عدس",
+    "unitPrice": 195000,
+    "packPrice": null,
+    "image": "Adas.JPG",
+    "order": 3,
+    "available": false
+  },
+  {
+    "id": 4,
+    "name": "لپه آذرشهر",
+    "unitPrice": 278000,
+    "packPrice": 1390000,
+    "image": "Lappe.jpg",
+    "order": 4,
+    "available": true
+  },
+  {
+    "id": 5,
+    "name": "نخود آبگوشتی",
+    "unitPrice": 239000,
+    "packPrice": null,
+    "image": "Nokhod.JPG",
+    "order": 5,
+    "available": true
+  }
+];
