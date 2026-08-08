447bc7fae70a00b10d40eb9ced422374d75035db
behzadbagheri8757-sudo <behzad.bagheri8757@gmail.com>
2026-08-08 19:09:32 +0330

    Add files via upload

---
+++ products.js
@@ -0,0 +1,195 @@
+// فایل اطلاعات محصولات
+// برای تغییر قیمت‌ها یا اضافه کردن محصول، این فایل را ویرایش کنید
+// یا از پنل مدیریت (admin.html) استفاده کرده و فایل جدید را دانلود و جایگزین کنید
+
+const products = [
+  {
+    "id": 1,
+    "name": "لوبیا چیتی",
+    "unitPrice": 395000,
+    "packPrice": null,
+    "image": "Lobia-chiti.JPG",
+    "order": 1,
+    "available": true
+  },
+  {
+    "id": 2,
+    "name": "عدس ساسکن",
+    "unitPrice": 235000,
+    "packPrice": 1175000,
+    "image": "adas-saskan.jpg",
+    "order": 2,
+    "available": true
+  },
+  {
+    "id": 3,
+    "name": "عدس",
+    "unitPrice": 195000,
+    "packPrice": null,
+    "image": "Adas.JPG",
+    "order": 3,
+    "available": true
+  },
+  {
+    "id": 4,
+    "name": "لپه آذرشهر",
+    "unitPrice": 278000,
+    "packPrice": 1390000,
+    "image": "Lappe.jpg",
+    "order": 4,
+    "available": true
+  },
+  {
+    "id": 5,
+    "name": "نخود آبگوشتی",
+    "unitPrice": 239000,
+    "packPrice": null,
+    "image": "Nokhod.JPG",
+    "order": 5,
+    "available": true
+  },
+  {
+    "id": 6,
+    "name": "لوبیا کشاورزی",
+    "unitPrice": 415000,
+    "packPrice": null,
+    "image": "Lobia-keahavarzi.JPG",
+    "order": 6,
+    "available": true
+  },
+  {
+    "id": 7,
+    "name": "لوبیا قرمز",
+    "unitPrice": 325000,
+    "packPrice": null,
+    "image": "Lobia-ghermez.JPG",
+    "order": 7,
+    "available": true
+  },
+  {
+    "id": 8,
+    "name": "سویا فله سبحان",
+    "unitPrice": 245000,
+    "packPrice": null,
+    "image": "Soya.JPG",
+    "order": 8,
+    "available": true
+  },
+  {
+    "id": 9,
+    "name": "ذرت پفیلا",
+    "unitPrice": 238000,
+    "packPrice": null,
+    "image": "Zorat.JPG",
+    "order": 9,
+    "available": true
+  },
+  {
+    "id": 10,
+    "name": "کشمش پلویی(کارتن ۹کیلویی)",
+    "unitPrice": 4780000,
+    "packPrice": null,
+    "image": "Keshmesh.JPG",
+    "order": 10,
+    "available": true
+  },
+  {
+    "id": 11,
+    "name": "مغز گردو صبحانه",
+    "unitPrice": 1210000,
+    "packPrice": null,
+    "image": "Gerdoo.JPG",
+    "order": 11,
+    "available": true
+  },
+  {
+    "id": 12,
+    "name": "تخمه دورسفید ویژه",
+    "unitPrice": 650000,
+    "packPrice": null,
+    "image": "Tokhme.JPG",
+    "order": 12,
+    "available": true
+  },
+  {
+    "id": 13,
+    "name": "تخمه کدو نمکی",
+    "unitPrice": 890000,
+    "packPrice": null,
+    "image": "Kadoo.JPG",
+    "order": 13,
+    "available": true
+  },
+  {
+    "id": 14,
+    "name": "تخمه کدو دوآتیشه",
+    "unitPrice": 910000,
+    "packPrice": null,
+    "image": "Kadoo2.JPG",
+    "order": 14,
+    "available": true
+  },
+  {
+    "id": 15,
+    "name": "زرشک پفکی",
+    "unitPrice": 830000,
+    "packPrice": null,
+    "image": "Zereshk.JPG",
+    "order": 15,
+    "available": true
+  },
+  {
+    "id": 16,
+    "name": "بادام زمینی آستانه",
+    "unitPrice": 640000,
+    "packPrice": null,
+    "image": "Badam.JPG",
+    "order": 16,
+    "available": false
+  },
+  {
+    "id": 17,
+    "name": "الو بخارا(بسته بندی نیم کیلویی)",
+    "unitPrice": 510000,
+    "packPrice": null,
+    "image": "Aloo.JPG",
+    "order": 17,
+    "available": true
+  },
+  {
+    "id": 18,
+    "name": "کشمش سبز ممتاز(کارتن ۵کیلویی)",
+    "unitPrice": 2950000,
+    "packPrice": null,
+    "image": "Keshmesh-sabz.JPG",
+    "order": 18,
+    "available": true
+  },
+  {
+    "id": 19,
+    "name": "ذرت پفیلا نورریز",
+    "unitPrice": 320000,
+    "packPrice": null,
+    "image": "Noriz.JPG",
+    "order": 19,
+    "available": true
+  },
+  {
+    "id": 20,
+    "name": "لیمو عمانی اعلاء",
+    "unitPrice": 680000,
+    "packPrice": null,
+    "image": "Limoo.JPG",
+    "order": 20,
+    "available": false
+  },
+  {
+    "id": 21,
+    "name": "لپه باقالا",
+    "unitPrice": 235000,
+    "packPrice": null,
+    "image": "Baghala.JPG",
+    "order": 21,
+    "available": true
+  }
+];
